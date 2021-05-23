dbe1a626183c801d9e7aaae4971f62d6bb30caeb
smta2019 <smta201911@gmail.com>
2021-05-23 13:58:25 +0900

    SecondKadaiAppをコミット。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingNameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.greetingName = greetingNameText.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
 
 
